25565b718f96865fa87ae389095c3cc86d065c8b
Fernando Marquardt <fernando.marquardt@gmail.com>
2018-05-10 18:21:56 -0300

    Webpack config updated.

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,113 @@
-var path = require('path');
-//var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var webpack = require('webpack');
-var DashboardPlugin = require('webpack-dashboard/plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+/* eslint-env node */
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-var rules = require('./webpack.rules');
+const devMode = process.env.NODE_ENV !== 'production';
 
-module.exports = function(env) {
-    return {
-        devtool: 'source-map',
-        resolve: {
-            extensions: [ '.js', '.jsx' ]
-        },
-        entry: [
-            'react-hot-loader/patch', // activate HMR for React
-            'webpack-dev-server/client?http://localhost:8080',
-            'webpack/hot/only-dev-server',
-            './app/index.jsx'
-        ],
-        output: {
-            filename: '[name].bundle.js',
-            path: path.resolve(__dirname, 'public'),
-            publicPath: '/'
-        },
-        devServer: {
-            hot: true, // enable HMR on the server,
-            inline: true,
-            noInfo: true, // don't print build stats,
-            historyApiFallback: true,
-            contentBase: path.resolve(__dirname, 'public'),
-            publicPath: '/'
-        },
-        module: {
-            rules
-        },
-        plugins: [
-            new webpack.HotModuleReplacementPlugin(), // enable HMR globally
-            new webpack.NamedModulesPlugin(),
-            new webpack.optimize.CommonsChunkPlugin({
-                name: 'vendor',
-                minChunks: function(module) {
-                    // Our vendor libraries come from node_modules
-                    return module.context && module.context.indexOf('node_modules') !== -1;
-                }
-            }),
-            // new ExtractTextPlugin({ filename: 'bundle.css', disable: false, allChunks: true }), // Add for production later
-            new DashboardPlugin(),
-            new HtmlWebpackPlugin({
-                template: './app/index.html'
-            })
+module.exports = {
+
+  devtool: 'cheap-module-source-map',
+
+  entry: path.resolve(__dirname, 'src/index.jsx'),
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  resolve: {
+    extensions: [ '.js', '.jsx', '.json' ]
+  },
+
+  devServer: {
+    contentBase: path.resolve(__dirname, 'assets'),
+    watchContentBase: true,
+    historyApiFallback: true,
+    hot: true,
+    https: true,
+    inline: true,
+    open: true
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'babel-loader',
+        options: {
+          cacheDirectory: true
+        }
+      }, {
+        test: /\.s?css$/,
+        exclude: path.resolve(__dirname, 'src/styles'),
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              camelCase: true
+            }
+          },
+          'postcss-loader',
+          'sass-loader'
         ]
-    };
+      }, {
+        test: /\.s?css$/,
+        include: path.resolve(__dirname, 'src/styles'),
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: false,
+              camelCase: true
+            }
+          }
+        ]
+      }, {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        exclude: /node_modules/,
+        use: 'file-loader'
+      }, {
+        test: /\.(woff|woff2)$/,
+        exclude: /node_modules/,
+        use: 'url-loader?prefix=font/&limit=5000'
+      }, {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        exclude: /node_modules/,
+        use: 'url-loader?limit=10000&mimetype=application/octet-stream'
+      }, {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        exclude: /node_modules/,
+        use: 'url-loader?limit=10000&mimetype=image/svg+xml'
+      }, {
+        test: /\.gif/,
+        exclude: /node_modules/,
+        use: 'url-loader?limit=10000&mimetype=image/gif'
+      }, {
+        test: /\.jpg/,
+        exclude: /node_modules/,
+        use: 'url-loader?limit=10000&mimetype=image/jpg'
+      }, {
+        test: /\.png/,
+        exclude: /node_modules/,
+        use: 'url-loader?limit=10000&mimetype=image/png'
+      }
+    ]
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css'
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html')
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
